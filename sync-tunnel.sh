ce5152706de9b1625f062261474fd4b96bf8db57
Jonathan Coates <git@squiddev.cc>
2022-01-09 11:27:41 +0000

    Strip as well as upx
    It's so boring waiting for this thing to sync :)

--- sync-tunnel.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-set -xe
-
-dune build -p scrutiny @install
-dune install --root=. --prefix _install
-scp -C _install/bin/scrutiny-infra-tunnel autoclave.squiddev.cc:.local/bin/scrutiny-infra-tunnel
