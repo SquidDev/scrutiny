6657a3304dbbabf59c7359a8cc55b534abebffff
Jonathan Coates <git@squiddev.cc>
2022-12-01 22:25:34 +0000

    Update to OCaml 5.0.0~beta2

--- flake.nix
+++ flake.nix
@@ -3,10 +3,6 @@
     opam-repository.url = "github:ocaml/opam-repository";
     opam-repository.flake = false;
 
-    # 5.0 patches
-    opam-repository-alpha.url = "github:kit-ty-kate/opam-alpha-repository";
-    opam-repository-alpha.flake = false;
-
     # Unlocks 5.0 on the main repository
     opam-repository-beta.url = "github:ocaml/ocaml-beta-repository";
     opam-repository-beta.flake = false;
@@ -20,30 +16,14 @@
 
   outputs = {
     self, utils, opam-nix, nixpkgs,
-    opam-repository, opam-repository-alpha, opam-repository-beta,
+    opam-repository, opam-repository-beta,
   }:
     let
       package = "scrutiny";
       repos = [
-        # opam-repository-alpha
         opam-repository-beta
         opam-repository
       ];
-
-      ocamlOverlay = final: prev: {
-        # https://github.com/tweag/opam-nix/issues/21
-        # ocaml = prev.ocaml.overrideAttrs(oa: {
-        #   nativeBuildInputs = oa.nativeBuildInputs ++ [final.ocaml-base-compiler];
-        # });
-
-        # Force a version with the hashes present
-        base = prev.base.overrideAttrs(oa: {
-          src = builtins.fetchTarball {
-            url = "https://github.com/kit-ty-kate/base/archive/500-015.tar.gz";
-            sha256 = "15vsiv3q53l1bzrvqgspf3lp2104s9dzw62z3nl75f53jvjvsyf6";
-          };
-        });
-      };
     in utils.lib.eachDefaultSystem (system:
       let
         opam = opam-nix.lib.${system};
@@ -53,14 +33,8 @@
           inherit pkgs repos;
 
           resolveArgs = { dev = false; };
-          overlays = [ocamlOverlay opam.defaultOverlay];
-        } package ./. {
-          ocaml-base-compiler = "*";
-
-          # The alpha repository is missing hashes, and so doesn't play well
-          # with flakes. Force an older version of ppx_deriving.
-          ppx_deriving = "5.2.1";
-        };
+          overlays = [opam.defaultOverlay];
+        } package ./. {};
         scrutiny = project.${package}.overrideAttrs(oa: {
           buildInputs = oa.buildInputs ++ [pkgs.systemd];
           nativeBuildInputs = oa.nativeBuildInputs ++ [pkgs.upx];
